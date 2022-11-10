75fc0afb26d36362a33acf6475ea1af11bd4b91f
Daniel Pathammavong <patha037@umn.edu>
2022-11-10 11:05:14 -0600

    Add ShelfList, ShelfForm, and ShelfItem

---
+++ src/components/ShelfItem/ShelfItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+
+
+function ShelfItem(){
+    return (
+        <>
+        <ul>
+            <li>  </li> <button> Delete </button>
+        </ul>
+        </>
+    )
+}
+
+export default ShelfItem;
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+//Component Imports
+import ShelfForm from '../ShelfForm/ShelfForm';
+import ShelfList from '../ShelfList/ShelfList';
+
+
+
 function ShelfPage() {
   return (
+    
     <div className="container">
       <h2>Shelf</h2>
+      <ShelfForm/>
+      <ShelfList/>
+      
       <p>All of the available items can be seen here.</p>
     </div>
   );
